204c97bb6f0bb4c377942129bc45abf85a10354e
GARIKINA GREESHMA <greeshmagarikina@gmail.com>
2026-07-08 22:21:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (63.25%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        for i in range(n-1):
+            if nums[i]==nums[i+1]:
+                nums[i]*=2
+                nums[i+1]=0
+        result=[x for x in nums if x!=0]
+        while len(result)<n:
+            result.append(0)
+        return result
