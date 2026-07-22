b4be9a8c00d89a0c06b8919f427d7ec6de86e5c1
GARIKINA GREESHMA <greeshmagarikina@gmail.com>
2026-07-22 21:25:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (17.37%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        if n==1 or n==2:
+            return 1
+        prev1=0
+        prev2=1
+        prev3=1
+        for _ in range(3,n+1):
+            current=prev1+prev2+prev3
+            prev1=prev2
+            prev2=prev3
+            prev3=current
+        return current
