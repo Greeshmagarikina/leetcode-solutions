de4585b2b0608a29f32ad4816ec4cba64936fbbe
GARIKINA GREESHMA <greeshmagarikina@gmail.com>
2026-07-24 22:11:24 +0530

    Time: 0 ms (100%), Space: 19.3 MB (49.52%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        balance=0
+        max=0
+        for char in s:
+            if char=='L':
+                balance+=1
+            else:
+                balance-=1
+            if balance==0:
+                max+=1
+        return max
