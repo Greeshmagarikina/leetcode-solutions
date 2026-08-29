e81702885cadaa23c7e0e4779d22820d93331a1c
GARIKINA GREESHMA <greeshmagarikina@gmail.com>
2026-08-29 13:45:45 +0530

    Time: 3 ms (30.09%), Space: 19.8 MB (63.27%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        three=set()
+        for num in nums:
+            three.add(num)
+            if len(three)>3:
+                three.remove(min(three))
+        if len(three)==3:
+            return min(three)
+        return max(three)
